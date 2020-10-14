0ae66dab2da994860afcc66b332e4b399abee899
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2020-10-14 14:20:04 +0500

    [Task-1] Remove unnecessary commented tests; Move corner checks to main.go

--- task-1/cmd/fibonacci/main.go
+++ task-1/cmd/fibonacci/main.go
@@ -10,7 +10,14 @@ var nFlag = flag.Int("n", 0, "index of required number (must be in range 0..20)"
 
 func main() {
 	flag.Parse()
-	result, err := fibonacci.At(*nFlag)
+
+	n := *nFlag
+	if n < 0 || n > 20 {
+		fmt.Println("sorry, we have paws instead of hands and so can only work with indexes in range 0..20")
+		return
+	}
+
+	result, err := fibonacci.At(n)
 	if err != nil {
 		fmt.Println(err)
 	} else {
