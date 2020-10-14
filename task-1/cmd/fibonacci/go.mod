1fd4585726d98d0312d5618b960b923e13cf3e03
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2020-10-14 12:38:34 +0500

    [Task-1] Adjust code after review: change fib algorithm, add modules, adjust tests

---
+++ task-1/cmd/fibonacci/go.mod
@@ -0,0 +1,6 @@
+module task-1/cmd/fibonacci
+
+require pkg/fibonacci v1.0.0
+replace pkg/fibonacci => ../../pkg/fibonacci
+
+go 1.15
--- task-1/pkg/fibonacci/fibonacci.go
+++ task-1/pkg/fibonacci/fibonacci.go
@@ -2,27 +2,26 @@
 package fibonacci
 
 import (
-	"log"
+	"errors"
 )
 
 // At calculates fibonacci value at specified index
-func At(n int) int {
-	if n < 0 || n > 19 {
-		log.Fatal("position value is invalid")
+func At(n int) (int, error) {
+	if n < 0 || n > 20 {
+		return 0, errors.New("sorry, we have paws instead of hands and so can only work with indexes in range 0..20")
 	} else if n == 0 {
-		return 0
+		return 0, nil
 	} else if n == 1 {
-		return 1
+		return 1, nil
 	}
 
-	values := make([]int, n+1, n+1)
-	values[1] = 1
+	first, second := 0, 1
 
 	for i := 2; i <= n; i++ {
-		values[i] = values[i-2] + values[i-1]
+		first, second = second, first + second
 	}
 
-	return values[n]
+	return second, nil
 }
 
 // Recursive At() variant
