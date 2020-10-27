6816a703891f60d52b528bfd149c4f295085bc9e
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2020-10-27 15:54:04 +0500

    [Task-5] Add package topology scheme; Implement and use binary tree as storage for records; Implement lists

--- task-5/pkg/index/index.go
+++ task-5/pkg/index/index.go
@@ -3,13 +3,12 @@ package index
 
 import (
 	"fmt"
-	"sort"
 	"strings"
 )
 
 // Index предоставляет методы для индексирования, а так же временно(!) выполняет функции базы данных (хранение и поиск)
 type Index struct {
-	storage       []Record
+	storage       BTree
 	invertedIndex map[string][]int
 	IdProvider    int
 }
@@ -21,10 +20,22 @@ type Record struct {
 	Title string
 }
 
+// BTree представляет собой бинарное дерево, предоставляющее методы по добавлению и поиску элементов
+type BTree struct {
+	root *node
+}
+
+// node представляет собой узел бинарного дерева. Этот тип хранит значение и ссылки на другие узлы
+type node struct {
+	left  *node
+	right *node
+	value *Record
+}
+
 // New создает новый экземпляр типа Index
 func New() *Index {
 	ind := Index{
-		storage:       []Record{},
+		storage:       BTree{},
 		invertedIndex: map[string][]int{},
 		IdProvider:    1,
 	}
@@ -32,25 +43,16 @@ func New() *Index {
 }
 
 // Search выполняет поиск по слову в хранилище с использованием инвертированного индекса
-// используется sort.Search(), который требует дополнительной проверки что коллекция содержит искомый элемент
 func (i *Index) Search(word string) []string {
 	found := []string{}
-	storageLength := len(i.storage)
 	ids, ok := i.invertedIndex[word]
 	if !ok {
 		return found
 	}
 
 	for _, id := range ids {
-		index := sort.Search(storageLength, func(ind int) bool {
-			return (i.storage[ind]).Id >= id
-		})
-
-		if index < storageLength {
-			rec := i.storage[index]
-			if rec.Id == id {
-				found = append(found, fmt.Sprintf("%s - %s", rec.Url, rec.Title))
-			}
+		if rec, ok := i.storage.Search(id); ok {
+			found = append(found, fmt.Sprintf("%s - %s", rec.Url, rec.Title))
 		}
 	}
 
@@ -59,40 +61,27 @@ func (i *Index) Search(word string) []string {
 
 // Fill заполняет хранилище и инвертированный индекс
 func (i *Index) Fill(data *map[string]string) {
-	i.fillStorage(data)
-	i.fillInvertedIndex()
-}
-
-// fillStorage заполняет хранилище элементами Record и выполняет сортировку storage по record.Id
-func (i *Index) fillStorage(data *map[string]string) {
 	for link, title := range *data {
 		rec := Record{
 			Id:    i.IdProvider,
 			Url:   link,
 			Title: title,
 		}
-		i.IdProvider++
-		i.storage = append(i.storage, rec)
-	}
+		i.storage.Add(&rec)
 
-	// Хотя данные уже отсортированы, все равно сортируем чтобы поиграть с сортировкой
-	sort.Slice(i.storage, func(a, b int) bool { return i.storage[a].Id < i.storage[b].Id })
-}
-
-// fillInvertedIndex разбирает record.Url и record.Title на примитивные лексемы и заполняет индекс
-// Индекс это ассоциативный массив, где ключ это лексема, а значение это массив состоящий из record.Id
-func (i *Index) fillInvertedIndex() {
-	for _, record := range i.storage {
 		lexemes := map[string]bool{} // map чтобы избежать дублей
-		parseLexemes(&lexemes, record.Url, "/", "#:")
-		parseLexemes(&lexemes, record.Title, " ", "()/,-")
+		parseLexemes(&lexemes, rec.Url, "/", "#:")
+		parseLexemes(&lexemes, rec.Title, " ", "()/,-")
 
 		for lex := range lexemes {
-			i.invertedIndex[lex] = append(i.invertedIndex[lex], record.Id)
+			i.invertedIndex[lex] = append(i.invertedIndex[lex], rec.Id)
 		}
+
+		i.IdProvider++
 	}
 }
 
+// parseLexemes разбирает текст на лексемы
 func parseLexemes(lexemes *map[string]bool, url, sep, tr string) {
 	for _, word := range strings.Split(url, sep) {
 		if tr != "" {
@@ -103,4 +92,75 @@ func parseLexemes(lexemes *map[string]bool, url, sep, tr string) {
 			(*lexemes)[word] = true
 		}
 	}
+}
+
+// Add позволяет добавить элемент в бинарное дерево
+func (bt *BTree) Add(r *Record) {
+	newNode := &node{
+		value: r,
+	}
+
+	if bt.root == nil {
+		bt.root = newNode
+		return
+	}
+
+	parentNode := bt.root
+	for {
+		if parentNode.value.Id > r.Id {
+			if parentNode.left == nil {
+				parentNode.left = newNode
+				break
+			}
+			parentNode = parentNode.left
+			continue
+		}
+
+		if parentNode.right == nil {
+			parentNode.right = newNode
+			break
+		}
+		parentNode = parentNode.right
+	}
+}
+
+// Search осуществляет поиск в бинарном дереве, второе возвращаемое значение равно false если запись не найдена
+func (bt *BTree) Search(id int) (*Record, bool) {
+	currentNode := bt.root
+
+	for {
+		if currentNode == nil {
+			return &Record{}, false
+		}
+
+		if currentNode.value.Id == id {
+			return currentNode.value, true
+		}
+
+		if currentNode.value.Id > id {
+			currentNode = currentNode.left
+			continue
+		}
+
+		currentNode = currentNode.right
+	}
+}
+
+// String позволяет получить простое строковое представление бинарного дерева
+func (bt *BTree) String() string {
+	elems := []int{}
+	bt.root.collect(&elems)
+	return fmt.Sprint(elems)
+}
+
+// collect выполняет рекурсивный обход дерева и собирает Id элементов в массив
+func (n *node) collect(s *[]int) {
+	if n == nil {
+		return
+	}
+
+	*s = append(*s, n.value.Id)
+
+	n.left.collect(s)
+	n.right.collect(s)
 }
--- task-5/pkg/index/index_test.go
+++ task-5/pkg/index/index_test.go
@@ -32,3 +32,55 @@ func TestSearch(t *testing.T) {
 		t.Errorf("len(found) = %d; want %d", got, want)
 	}
 }
+
+func TestBTree_Add(t *testing.T) {
+	tree := BTree{}
+
+	got := tree.String()
+	want := "[]"
+	if got != want {
+		t.Errorf("tree.String() = %s; want %s", got, want)
+	}
+
+	tree.Add(&Record{Id: 5})
+	got = tree.String()
+	want = "[5]"
+	if got != want {
+		t.Errorf("tree.String() = %s; want %s", got, want)
+	}
+
+	tree.Add(&Record{Id: 3})
+	tree.Add(&Record{Id: 8})
+	tree.Add(&Record{Id: 4})
+	tree.Add(&Record{Id: 1})
+	got = tree.String()
+	want = "[5 3 1 4 8]"
+	if got != want {
+		t.Errorf("tree.String() = %s; want %s", got, want)
+	}
+}
+
+func TestBTree_Search(t *testing.T) {
+	tree := BTree{}
+	rec := &Record{Id: 4}
+
+	tree.Add(&Record{Id: 5})
+	tree.Add(&Record{Id: 3})
+	tree.Add(&Record{Id: 8})
+	tree.Add(rec)
+	tree.Add(&Record{Id: 1})
+
+	got, ok := tree.Search(4)
+	if !ok {
+		t.Errorf("ok in tree.String() = %v; want true", ok)
+	}
+	want := rec
+	if got != want {
+		t.Errorf("tree.String() = %v; want %v", got, want)
+	}
+
+	_, ok = tree.Search(7)
+	if ok {
+		t.Errorf("ok in tree.String() = %v; want false", ok)
+	}
+}
